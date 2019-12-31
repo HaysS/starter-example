52c27082326011f8a6de3fe49cf0a1e0f22b4258
Hays Stanford <stanfordhays@gmail.com>
2019-12-31 14:57:44 -0600

    added descriptive comment to example

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << "Hello World!" << std::endl;
+
+  return 0; 
+}
+
+/*
+	Compile & run this using the code below in your CLI:
+	
+	g++ -o helloWorld helloWorld.cpp && ./helloWorld
+*/
